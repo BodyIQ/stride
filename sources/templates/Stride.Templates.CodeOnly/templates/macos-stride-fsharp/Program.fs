a09cf3ce920ca9b31ce62eab39b19980534f3b77
Gurdas Nijor <nijor22@gmail.com>
2026-07-09 18:12:56 -0700

    Remove toolkit dependency from macOS F# template

--- sources/templates/Stride.Templates.CodeOnly/templates/macos-stride-fsharp/Program.fs
+++ sources/templates/Stride.Templates.CodeOnly/templates/macos-stride-fsharp/Program.fs
@@ -1,22 +1,69 @@
-open Stride.CommunityToolkit.Engine
-open Stride.CommunityToolkit.Games
-open Stride.CommunityToolkit.Rendering.ProceduralModels
-open Stride.CommunityToolkit.Skyboxes
 open Stride.Core.Mathematics
 open Stride.Engine
+open Stride.Games
+open Stride.Graphics
+open Stride.Rendering
+open Stride.Rendering.Compositing
+open Stride.Rendering.Lights
+open Stride.Rendering.Materials
+open Stride.Rendering.Materials.ComputeColors
+open Stride.Rendering.ProceduralModels
 
-let game = new Game()
+let createMaterial (game: Game) (color: Color) =
+    let descriptor = MaterialDescriptor()
+    descriptor.Attributes.Diffuse <- MaterialDiffuseMapFeature(ComputeColor(color))
+    descriptor.Attributes.DiffuseModel <- MaterialDiffuseLambertModelFeature()
+    Material.New(game.GraphicsDevice, game.Content, descriptor)
 
-let start (rootScene: Scene) =
-    game.SetupBase3D()
-    game.Add3DCameraController() |> ignore
-    game.AddSkybox() |> ignore
+let addCamera (game: Game) (scene: Scene) =
+    let cameraComponent = CameraComponent()
+    game.SceneSystem.GraphicsCompositor <-
+        GraphicsCompositorHelper.CreateDefault(
+            false,
+            camera = cameraComponent,
+            clearColor = System.Nullable<Color4>(Color4(0.52f, 0.70f, 0.95f, 1.0f)),
+            graphicsProfile = GraphicsProfile.Level_10_0)
 
-    let capsule = game.Create3DPrimitive(PrimitiveModelType.Capsule)
-    capsule.Transform.Position <- Vector3(0f, 2.5f, 0f)
-    capsule.Scene <- rootScene
+    let camera = new Entity("Camera")
+    camera.Add(cameraComponent)
+    camera.Transform.Position <- Vector3(0.0f, 3.0f, -6.0f)
+    let mutable forward = Vector3.Normalize(Vector3(0.0f, -0.25f, 1.0f))
+    let mutable up = Vector3.UnitY
+    camera.Transform.Rotation <- Quaternion.LookRotation(&forward, &up)
+    scene.Entities.Add(camera)
+
+let addLight (scene: Scene) =
+    let light = new LightComponent()
+    light.Type <- new LightDirectional()
+    light.Intensity <- 1.0f
+
+    let entity = new Entity("Sun")
+    entity.Add(light)
+    entity.Transform.Rotation <- Quaternion.RotationYawPitchRoll(-0.6f, -0.75f, 0.0f)
+    scene.Entities.Add(entity)
+
+let addCube (game: Game) (scene: Scene) =
+    let model = CubeProceduralModel(Size = Vector3(1.5f, 1.5f, 1.5f)).Generate(game.Services)
+    model.Materials.Add(createMaterial game (Color(245uy, 176uy, 65uy, 255uy)))
+
+    let entity = new Entity("Cube")
+    entity.Transform.Position <- Vector3(0.0f, 1.0f, 0.0f)
+    entity.Add(ModelComponent(model))
+    scene.Entities.Add(entity)
+
+type MyTemplateGame() =
+    inherit Game()
+
+    override this.BeginRun() =
+        base.BeginRun()
+
+        let scene = this.SceneSystem.SceneInstance.RootScene
+        addCamera this scene
+        addLight scene
+        addCube this scene
 
 [<EntryPoint>]
 let main _ =
-    game.Run(start = start)
+    use game = new MyTemplateGame()
+    game.Run()
     0
